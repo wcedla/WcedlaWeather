4591c4920a22a2e70f23e4815b5ed6fee28d9423
wcedla <wcedla@live.com>
2018-09-12 20:14:47 +0800

    添加数据库省份城市空表

---
+++ app/src/main/java/com/wcedla/wcedlaweather/db/County.java
@@ -0,0 +1,43 @@
+package com.wcedla.wcedlaweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class County extends LitePalSupport {
+
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private String cityCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+}
